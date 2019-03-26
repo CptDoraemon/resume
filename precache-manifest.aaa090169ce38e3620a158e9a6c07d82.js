b092b07fc5b8ca0406a600460a4fc1a3c36871f7
CptDoraemon <yxx0827@gmail.com>
2019-03-26 09:55:04 -0400

    Updates

---
+++ precache-manifest.aaa090169ce38e3620a158e9a6c07d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "a3e94fa22ee6ca1f7a29",
+    "url": "/resume/static/js/main.d522d3e2.chunk.js"
+  },
+  {
+    "revision": "08e2e9bb3ef7ecad13f4",
+    "url": "/resume/static/js/2.06d60815.chunk.js"
+  },
+  {
+    "revision": "a3e94fa22ee6ca1f7a29",
+    "url": "/resume/static/css/main.160fe56e.chunk.css"
+  },
+  {
+    "revision": "4fb475b90ace9f69c3a455ac64ea327b",
+    "url": "/resume/index.html"
+  }
+];
