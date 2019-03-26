59f77a47cfa2342fb20ddb5c4449d1a2ffdb5a0e
CptDoraemon <yxx0827@gmail.com>
2019-03-26 09:53:47 -0400

    darken colors

---
+++ build/precache-manifest.40e05f71e5c35c77db957b609d21cabd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "79cdd8e3d321fd184675",
+    "url": "./static/js/main.d522d3e2.chunk.js"
+  },
+  {
+    "revision": "08e2e9bb3ef7ecad13f4",
+    "url": "./static/js/2.06d60815.chunk.js"
+  },
+  {
+    "revision": "79cdd8e3d321fd184675",
+    "url": "./static/css/main.4b836957.chunk.css"
+  },
+  {
+    "revision": "6db3876e0151d9c5e431fb4b2233859e",
+    "url": "./index.html"
+  }
+];
